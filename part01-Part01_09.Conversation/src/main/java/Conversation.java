5b4c2622835533355de16cf02e9431ebccd0dc23
Bram <Bram.mathijssen@gmail.com>
2020-08-05 01:17:35 +0200

    exercise 6-13 finished

--- part01-Part01_09.Conversation/src/main/java/Conversation.java
+++ part01-Part01_09.Conversation/src/main/java/Conversation.java
@@ -7,6 +7,15 @@ public class Conversation {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Greetings! How are you doing?");
+        
+        String message = scanner.nextLine();
+        
+        System.out.println("Oh, how interesting. Tell me more!");
+        
+        String message2 = scanner.nextLine();
+        
+        System.out.println("Thanks for sharing!");
 
     }
 }
