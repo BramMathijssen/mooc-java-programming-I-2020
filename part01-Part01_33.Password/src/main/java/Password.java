1fa116a486843847a355a5077a32b58fe6cdf679
Bram <Bram.mathijssen@gmail.com>
2020-08-08 01:29:43 +0200

    exercise 30-37 finished. Part 1 done

--- part01-Part01_33.Password/src/main/java/Password.java
+++ part01-Part01_33.Password/src/main/java/Password.java
@@ -5,7 +5,17 @@ public class Password {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String password = "Caput Draconis";
 
-        // Write your program here 
+        // Write your program here
+        System.out.println("Password?");
+        String input = scan.nextLine();
+        
+        if(input.equals(password)){
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Off with you!");
+        }
+        
     }
 }
--- part01-Part01_37.GiftTax/src/main/java/GiftTax.java
+++ part01-Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -5,6 +5,24 @@ public class GiftTax {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        
+        System.out.println("Value of the gift?");
+        int giftPrice = Integer.valueOf(scan.nextLine());
+        
+        if ( giftPrice < 5000){
+            System.out.println("No tax!");
+        } else if( giftPrice >= 5000 && giftPrice <25000){
+            System.out.println((100 +(giftPrice - 5000)* 0.08));
+        } else if( giftPrice >= 25000 && giftPrice <55000){
+            System.out.println((1700 +(giftPrice - 25000)* 0.1));
+        } else if( giftPrice >= 55000 && giftPrice <200000){
+            System.out.println((4700 +(giftPrice - 55000)* 0.12));
+        } else if( giftPrice >= 200000 && giftPrice <1000000){
+            System.out.println((22100 +(giftPrice - 200000)* 0.15));
+        } else if( giftPrice >= 1000000){
+            System.out.println((142100 +(giftPrice - 1000000)* 0.17));
+        }
+        
+             
     }
 }
