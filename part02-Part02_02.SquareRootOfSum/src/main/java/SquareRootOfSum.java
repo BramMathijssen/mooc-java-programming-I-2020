63ee67ad782d204dee4a206524160da3848ff7af
Bram <Bram.mathijssen@gmail.com>
2020-08-08 02:51:15 +0200

    part 2 exercise 1-7 finished

--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,15 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int input = Integer.valueOf(scanner.nextLine());
+        int input2 = Integer.valueOf(scanner.nextLine());
+        
+        int total = input + input2;
+        
+        double squareRoot = Math.sqrt(total);
+        
+        System.out.println(squareRoot);
 
     }
 }
--- part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -5,6 +5,20 @@ public class OnlyPositives {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        while(true){
+            System.out.println("Give a number: ");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if(number > 0 ){
+                int power = number * number;
+                System.out.println(power);
+            } else if (number < 0 ){
+                System.out.println("Unsuitable number");      
+            } else if (number == 0){
+                break;
+            }
+        }
 
     }
 }
