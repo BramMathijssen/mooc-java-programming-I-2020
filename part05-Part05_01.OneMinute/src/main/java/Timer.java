778545869b01a8dc7bdd61e6244287053092a75f
Bram <Bram.mathijssen@gmail.com>
2020-09-17 21:42:17 +0200

    part 5 exercise 1 finished

---
+++ part05-Part05_01.OneMinute/src/main/java/Timer.java
@@ -0,0 +1,31 @@
+
+public class Timer {
+    private ClockHand hundreth;
+    private ClockHand seconds;
+
+    public Timer() {
+        this.hundreth = new ClockHand(100);
+        this.seconds = new ClockHand(60);
+    }
+    
+    public void advance(){
+        this.hundreth.advance();
+        
+        if(this.hundreth.value()== 0){
+            this.seconds.advance();
+        }     
+    }
+
+    @Override
+    public String toString() {
+        return  seconds + ":" + hundreth;
+    }
+    
+    
+    
+
+    
+    
+    
+    
+}
